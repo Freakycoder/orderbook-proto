e4a9de7c88118087b26b989f18ba3964f37d8e19
Ahmed <114143727+Freakycoder@users.noreply.github.com>
2026-02-21 21:25:12 +0400

    implemented from trait for bookdepth

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,20 @@ pub use orders::{
     NewOrderRequest, NewOrderResponse, BookRequest, BookResponse,BookDepth,PriceLevelDepth,
     order_book_client::{OrderBookClient}
 };
+
+impl From<clob_engine::order_book::types::BookDepth> for BookDepth {
+     fn from(value: clob_engine::order_book::types::BookDepth) -> Self {
+        let bid_depth = value.bid_depth.into_iter()
+        .map(|price_level_depth| PriceLevelDepth {
+            price : price_level_depth.price_level,
+            quantity : price_level_depth.quantity
+        }).collect();
+
+        let ask_depth = value.ask_depth.into_iter()
+        .map(|price_level_depth| PriceLevelDepth {
+            price : price_level_depth.price_level,
+            quantity : price_level_depth.quantity
+        }).collect();
+        Self { bid_depth, ask_depth }
+    }
+}
